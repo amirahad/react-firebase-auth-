29ca1e496c69d7486cd653664647d552732443e5
amirahad <arahad4783@gmail.com>
2022-10-20 02:21:20 +0600

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import initializeAuthentication from './firebase/firebase.initialize';
-import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, signOut } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, signOut } from "firebase/auth";
 import { useState } from 'react';
 
 
@@ -17,11 +17,17 @@ const facebookProvider = new FacebookAuthProvider();
 
 function App() {
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
   const [user, setUser] = useState({});
   const auth = getAuth();
 
 
   const handleGoogleSignIn = () => {
+
+
 
     signInWithPopup(auth, googleProvider)
       .then(result => {
@@ -51,7 +57,7 @@ function App() {
         };
         setUser(loggedInUser);
       }).catch(error => {
-        console.log(error.message);
+        setError(error.message);
       });
   }
 
@@ -69,26 +75,102 @@ function App() {
         // };
         setUser(loggedInUser);
       }).catch(error => {
-        console.log(error.message);
+        setError(error.message);
       });
   }
+
+
+
+  const handleRegistration = e => {
+    e.preventDefault();
+    if (password.length < 6) {
+      setError('Password must be at least 6 characters long');
+      return;
+    }
+
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setError('Password must contain 2 upper case');
+      return;
+    }
+
+
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      }).catch(error => {
+        setError(error.message);
+      });
+
+  }
+
 
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         setUser({});
       }).catch((error) => {
-        console.log(error.message);
+        setError(error.message);
       });
   }
 
+  // const handleEmailChange = e => {
+  //   setEmail(e.target.value);
+  // }
+
+  // const handlePassChange = e => {
+  //   setPassword(e.target.value);
+  // }
 
 
 
 
   return (
     <div className="App">
-      <h1>TESTING</h1>
+      <h1>TESTING AUTHENTICATION</h1>
+      <form className='my-4' onSubmit={handleRegistration}>
+        <div className="form-group row mx-auto">
+          <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-8">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder='Email'
+              onBlur={(e) => {
+                setEmail(e.target.value);
+              }}
+              required
+            />
+          </div>
+        </div>
+        <div className="form-group row mx-auto">
+          <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-8">
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              required
+            />
+          </div>
+        </div>
+        {
+          error && <p className='text-danger'>{error}</p>
+        }
+
+        {/* <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div> */}
+        <input type="submit" value="Register" />
+      </form>
+
       {
         !user.name ? <div>
           <button onClick={handleGoogleSignIn}>Google Sign in</button>
